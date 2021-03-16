1ee05e0b8047291603ed3a793ab5bbec6ce64a5a
MokhtariNouhaila <nouhailamokhtari4@gmail.com>
2021-03-16 15:38:28 +0100

    page favoris

--- src/pages/ach_Favoris.js
+++ src/pages/ach_Favoris.js
@@ -4,12 +4,16 @@ import { Link } from "react-router-dom";
 import Pagination from "react-js-pagination";
 import "bootstrap-less";
 import Loader from "react-loader-spinner";
+import Swal from "sweetalert2";
+import { GiWeight, GiSheep } from 'react-icons/gi';
+import { HiOutlineBadgeCheck } from 'react-icons/hi';
 
 class Commandes extends Component {
   constructor() {
     super();
     // let redirect = false;
     this.state = {
+      nbr:null,
       loading: true,
       activePage: 1,
       nombrePages: [],
@@ -18,8 +22,13 @@ class Commandes extends Component {
       Favoris: [],
       redirect: false,
       activePage: 15,
+      panier: [],
+       idp:[],
     };
     // this.elv = this.elv.bind(this);
+    this.handleDeleteFromFavoris = this.handleDeleteFromFavoris.bind(this);
+    this.handlePanier = this.handlePanier.bind(this);
+
   }
   // elv = (id) => {
   //   axios
@@ -54,49 +63,172 @@ class Commandes extends Component {
       ":" +
       appendLeadingZeroes(current_datetime.getSeconds());
 
-    console.log(formatted_date);
-
     const expiredTimeToken = localStorage.getItem("expiredTimeToken");
     const token = localStorage.getItem("usertoken");
     const myToken = `Bearer ` + localStorage.getItem("myToken");
-    console.log(expiredTimeToken);
 
     if (!token || expiredTimeToken < formatted_date) {
       this.props.history.push("/login");
     } else {
-      this.setState({ loading: true }, () => {  
+      this.setState({ loading: true }, () => {
+        axios
+          .get("http://127.0.0.1:8000/api/consommateur/" + token + "/favoris", {
+            headers: {
+              // "x-access-token": token, // the token is a variable which holds the token
+              // "Content-Type": "application/json",
+              Authorization: myToken,
+            },
+          })
+          .then((res) => {
+            this.setState(
+              {
+                Favoris: res.data,
+                loading: false,
+              },
+              () => console.log("in call" + this.state.Favoris)
+            );
+            const pageNumbers = [];
+            for (
+              let i = 1;
+              i <=
+              Math.ceil(this.state.Favoris.length / this.state.annoncesPerPage);
+              i++
+            ) {
+              pageNumbers.push(i);
+            }
+            this.setState({ nombrePages: pageNumbers });
+          });
+      });
+    }
+
+
+
+    this.setState(() => {
       axios
-        .get("http://127.0.0.1:8000/api/consommateur/" + token + "/favoris", {
+        .get("http://127.0.0.1:8000/api/consommateur/" + token + "/panier", {
           headers: {
             // "x-access-token": token, // the token is a variable which holds the token
-            // "Content-Type": "application/json",
+            "Content-Type": "application/json",
             Authorization: myToken,
           },
         })
         .then((res) => {
+
           this.setState(
             {
-              Favoris: res.data,
-              loading: false,
-            },
-            () => console.log("in call" + this.state.Favoris)
+              panier: res.data,
+
+            }
           );
-          const pageNumbers = [];
-          for (
-            let i = 1;
-            i <=
-            Math.ceil(this.state.Favoris.length / this.state.annoncesPerPage);
-            i++
-          ) {
-            pageNumbers.push(i);
+          let p=[];
+this.state.panier.map((e)=>{p.push(e._id)}) 
+this.setState( { idp:p })
+         });
+
+    });
+  }
+  annonceVision(a) {
+    if (a.race === undefined) {
+      return " ";
+    } else return a.race;
+  }
+  ispanier(a) {
+   // let find = false;
+   // let i = 0;
+   // while (!find && i < this.state.idp.length) {
+  //    if (this.state.idp[i] == a) { find = true; break }
+   //   else { find = false; }
+   //   i++;
+   // }
+   // return find;
+   return this.state.idp.includes(a);
+  }
+  handlePanier(Mid) {
+    const token = localStorage.getItem("usertoken");
+    const myToken = `Bearer ` + localStorage.getItem("myToken");
+    if (!token) {
+      this.props.history.push("/login");
+    } else {
+      // console.log(token);
+      axios
+        .put(
+          "http://127.0.0.1:8000/api/consommateur/" + token + "/panier",
+          { id_espece: Mid },
+
+          {
+            headers: {
+              // "Access-Control-Allow-Origin": "*",
+              // "Content-Type": "application/json",
+              // Accept: "application/json",
+              "Authorization": myToken,
+            },
           }
-          this.setState({ nombrePages: pageNumbers });
-        });});
+        )   
+        .then(this.setState({ nbr:this.state.idp.push(Mid) ,idp:this.state.idp }));      
+        
+ 
+      Swal.fire({
+        title: "Ajouté dans Pannier",
+        icon: "success",
+        width: 400,
+        heightAuto: false,
+        timer: 1500,
+        showConfirmButton: false,
+        /* confirmButtonColor: "#7fad39",
+
+        confirmButtonText: "Ok!",*/
+      });
+    }
+  }
+  handleDeleteFromFavoris(Mid) {
+    // const idm = this.props.location.state.id;
+    // console.log(Mid);
+    const token = localStorage.getItem("usertoken");
+    const myToken = `Bearer ` + localStorage.getItem("myToken");
+    if (!token) {
+      this.props.history.push("/login");
+    } else {
+
+
+      axios
+        .put(
+          "http://127.0.0.1:8000/api/consommateur/" + token + "/favoris/" + Mid,
+          {},
+          {
+            headers: {
+              //"Content-Type": "application/json",
+
+              Authorization: myToken,
+            },
+          }
+        )
+        .then(() => {
+          this.setState(
+            {
+              Favoris: this.state.Favoris.filter(
+                (Favoris) => Favoris._id !== Mid
+              ),
+            });
+          this.props.history.push("/Favoris")
+
+        });
+      Swal.fire({
+        title: "Supprimé avec succès ",
+        icon: "success",
+        width: 400,
+        heightAuto: false,
+        timer: 1500,
+        showConfirmButton: false,
+        /* confirmButtonColor: "#7fad39",
+ 
+        confirmButtonText: "Ok!",*/
+      });
+
+
     }
   }
 
   handlePageChange(pageNumber) {
-    console.log(`active page is ${pageNumber}`);
     this.setState({ activePage: pageNumber });
   }
 
@@ -106,16 +238,15 @@ class Commandes extends Component {
 
   render() {
     var fav = this.state.Favoris.filter((Favoris) => Favoris !== null);
-     const indexOfLastAnnonce =
-       this.state.currentPage * this.state.annoncesPerPage;
-     const indexOfFirstAnnonce =
-       indexOfLastAnnonce - this.state.annoncesPerPage;
-     const currentAnnonces = this.state.Favoris.slice(
-       indexOfFirstAnnonce,
-       indexOfLastAnnonce
-     );
-     const { loading } = this.state;
-    console.log(fav);
+    const indexOfLastAnnonce =
+      this.state.currentPage * this.state.annoncesPerPage;
+    const indexOfFirstAnnonce =
+      indexOfLastAnnonce - this.state.annoncesPerPage;
+    const currentAnnonces = this.state.Favoris.slice(
+      indexOfFirstAnnonce,
+      indexOfLastAnnonce
+    );
+    const { loading } = this.state;
     let titre;
     if (fav.length == 1 || fav.length == 0) {
       titre = (
@@ -143,7 +274,7 @@ class Commandes extends Component {
               Mes favoris <i className="fa fa-heart"> </i>
             </h3>
             <div className="row">
-              
+
               <div className="col-lg-12 col-md-7">
                 {/*<!-- Sheeps Grid Section Begin --> */}
                 <div className="filter__found text-left">
@@ -152,66 +283,128 @@ class Commandes extends Component {
                   </h6>
                 </div>
                 {loading ? (
-                    <div
-                      style={{
-                        width: "100%",
-                        height: "100",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                      }}
-                    >
-                      <Loader
-                        type="Oval"
-                        color="#7fad39"
-                        height="80"
-                        width="80"
-                      />
-                    </div>
-                  ) : (
-                <div>
-                <div class="row">
-                  {fav.map((Annonces) => (
-                    //  {if(Annonces){}}
-                    <div className="col-lg-3 col-md-3 col-sm-6">
-                      {console.log(Annonces)}
-                      <div id="anonce" className="product__item">
-                        <div
-                          className="product__item__pic set-bg"
-                          // data-setbg={Annonces.images}
-                          // src="Images/sardi1.jpg"
-                        >
-                          <centre>
-                            {" "}
-                            <img
-                              src={Annonces.image_face}
+                  <div
+                    style={{
+                      width: "100%",
+                      height: "100",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Loader
+                      type="Oval"
+                      color="#7fad39"
+                      height="80"
+                      width="80"
+                    />
+                  </div>
+                ) : (
+                  <div>
+                    <div class="row">
+                      {fav.map((Annonces) => (
+                        //  {if(Annonces){}}
+                        <div className="col-lg-3 col-md-3 col-sm-6">
+                          <div id="anonce" className="product__item">
+                            <div
                               className="product__item__pic set-bg"
-                            />
-                          </centre>
-                          <ul class="product__item__pic__hover">
-                            <li>
-                              <Link to={`/DetailsMouton/${Annonces._id}`}>
-                                <a href="#">
-                                  <i class="fa fa-eye"></i>
-                                </a>
-                              </Link>
-                            </li>
-                          </ul>
+                            // data-setbg={Annonces.images}
+                            // src="Images/sardi1.jpg"
+                            >
+                              <centre>
+                                {" "}
+                                <img
+                                  src={Annonces.image_face}
+                                  className="product__item__pic set-bg"
+                                />
+                              </centre>
+                              <ul class="product__item__pic__hover">
+                                <li>
+                                  <Link to={`/DetailsMouton/${Annonces._id}`}>
+                                    <a href="#">
+                                      <i class="fa fa-eye"></i>
+                                    </a>
+                                  </Link>
+                                </li>
+                                <li>
+                                  <a
+                                    id={Annonces._id}
+                                    onClick={(e) =>
+                                      this.handleDeleteFromFavoris(
+                                        e.currentTarget.id
+                                      )
+                                    }
+                                  >
+                                    <i class="fa fa-trash"></i>
+                                  </a>
+                                </li>
+                              </ul>
+                            </div>
+                            {Annonces.anoc ?
+                              <h1 style={{ borderRadius: "0% 0% 0% 40%", fontSize: "14px" }} class=" badge badge-success pt-2 w-100  ">
+                                <HiOutlineBadgeCheck className=" mr-1 fa-lg " />
+                                <span>Labélisé ANOC</span>  </h1>
+                              :
+                              <span className="badge pt-3 w-100 mb-2    ">{" "}</span>
+                            }
+
+                            <div className="product__item__text p-2 text-justify">
+                              <h6 ><GiSheep className=" mr-1 fa-lg " />{Annonces.categorie}
+
+                                <span className="float-right">
+                                  <i class="fa fa-dot-circle-o"></i> {this.annonceVision(Annonces)}
+                                </span> </h6>
+
+                              <h6><GiWeight className=" mr-1 fa-lg " />
+                                {Annonces.poids + " Kg"}
+                                <img style={{ width: "10%", marginLeft: "91px", marginBottom: "10px" }} src="./Images/age.png"></img>
+
+                                <span className="float-right mt-1">
+                                  {Annonces.age + " mois"}</span></h6>
+
+                              <h5 id="mad">
+                                {"         " + Annonces.prix + " MAD"}
+                              </h5>
+
+
+                              {Annonces.statut == "disponible" ?
+                                (<div>
+                                  {!this.ispanier(Annonces._id) ?
+                                    <button
+                                      id={Annonces._id}
+                                      className="float-right rounded text-white bg-success py-1 px-2  "
+                                      style={{   fontSize: "16px",  border: "none" }}
+                                      onClick={(e) => {
+                                        this.handlePanier(e.currentTarget.id);
+                                      }
+                                      }
+                                    >
+                                      <i className="fa fa-shopping-cart "> {""} ajouter au Panier</i>
+                                    </button>
+                                    : <button
+                                      id={Annonces._id}
+                                      disabled="disabled"
+                                      className="float-right rounded text-white   btn btn-default py-1 px-2  "
+                                      style={{   fontSize: "16px",    border: "none" }}
+                                    >
+                                      <i className="fa fa-shopping-cart "> {""} ajouter au Panier</i>
+                                    </button>}
+                                </div>)
+                                : (<button
+                                  id={Annonces._id}
+                                  disabled="disabled"
+                                  className="float-right rounded text-white  btn btn-default py-1 px-2  "
+                                  style={{   fontSize: "16px",    border: "none" }}
+                                >
+                                  <i className="fa fa-shopping-cart "> {""} ajouter au Panier</i>
+                                </button>)}<div>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                        <div className="product__item__text">
-                          {/* <h6 className="text-danger">{"         " + Annonces.statut}</h6> */}
-                          {/* <h6>{"   Livrer à :      " + Annonces.point_relais}</h6> */}
-                          {/* <h6> {"         " + Annonces.prix + "  MAD"}</h6> */}
-                          <h5>{"         " + Annonces.poids + " Kg"}</h5>
-                          <h5 id="prixFav">
-                            {"         " + Annonces.prix + " MAD"}
-                          </h5>
-                        </div>
-                      </div>
+                      ))}
                     </div>
-                  ))}
-                </div>
-                </div>)}
+                  </div>)}
                 <div className="center-div">
                   <nav className="row">
                     <ul className="pagination center-div">
@@ -228,10 +421,10 @@ class Commandes extends Component {
                     </ul>
                   </nav>
                 </div>
-                 
+
                 <br></br>
                 {/* <!-- Sheeps Grid Section End --> */}
-              </div> 
+              </div>
             </div>
           </div>
         </section>

8f98f5a6ef4db73e835c051a324a020478b2d083
MokhtariNouhaila <nouhailamokhtari4@gmail.com>
2021-03-22 14:53:56 +0100

    page panier

--- src/pages/ach_Panier.js
+++ src/pages/ach_Panier.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
+import { HiOutlineBadgeCheck } from 'react-icons/hi';
 
 import { Link } from "react-router-dom";
 import Loader from "react-loader-spinner";
+
+import { GiWeight, GiSheep } from 'react-icons/gi';
 class Commandes extends Component {
   constructor() {
     super();
@@ -13,12 +16,12 @@ class Commandes extends Component {
       activePage: 1,
       nombrePages: [],
       currentPage: 1,
-      annoncesPerPage: 5,
-      Favoris: [],
+      annoncesPerPage: 2,
+      Paniers: [],
+      panier: [],
       redirect: false,
-      // mouton: {},
-      // showAvance: false,
-      // showReste: false,
+      coop: [],
+      coopn: [],
     };
     // this.elv = this.elv.bind(this);
     this.handleDeleteFromPanier = this.handleDeleteFromPanier.bind(this);
@@ -56,12 +59,10 @@ class Commandes extends Component {
       ":" +
       appendLeadingZeroes(current_datetime.getSeconds());
 
-    console.log(formatted_date);
 
     const expiredTimeToken = localStorage.getItem("expiredTimeToken");
     const token = localStorage.getItem("usertoken");
     const myToken = `Bearer ` + localStorage.getItem("myToken");
-    console.log(expiredTimeToken);
 
     if (!token || expiredTimeToken < formatted_date) {
       this.props.history.push("/login");
@@ -75,36 +76,61 @@ class Commandes extends Component {
               Authorization: myToken,
             },
           })
-          .then((res) => {            
+          .then((res) => {
 
             this.setState(
               {
-                Favoris: res.data,
+                Paniers: res.data,
                 loading: false,
               },
               () =>
                 this.setState({
-                  Favoris: this.state.Favoris.filter(
-                    (Favoris) => Favoris.statut === "disponible"
+                  Paniers: this.state.Paniers.filter(
+                    (Paniers) => Paniers.statut === "disponible"
                   ),
                 })
             );
+            let i = 0; let coop = [];
+            this.state.Paniers.map((c) => { coop[i] = c.id_cooperative; i++ });
+            const unique = (value, index, self) => { return self.indexOf(value) === index }
+            this.setState({ coop: coop.filter(unique) });
+            let coopn = [];
+            this.state.coop.map((c) => (
+              axios
+                .get("http://127.0.0.1:8000/api/cooperative/" + c, {
+                  headers: {
+                    "Content-Type": "application/json",
+                    Authorization: myToken,
+                  },
+                })
+                .then((res) => {
+                  coopn.splice(0, 0, { "nom": res.data.nom, "idc": c });
+                  this.setState({ coopn: coopn });
+                  let p = [];
 
-            const pageNumbers = [];
-            for (
-              let i = 1;
-              i <=
-              Math.ceil(this.state.Favoris.length / this.state.annoncesPerPage);
-              i++
-            ) {
-              pageNumbers.push(i);
-            }
-            this.setState({ nombrePages: pageNumbers });
+                  this.state.coop.map((c) => {
+                    p.splice(0, 0, {
+                      "id_coop": c,
+                      "nom_coop": coopn.filter((f) => (f.idc == c)).map((m) => m.nom)[0],
+                      "id_espaces": this.state.Paniers.filter((f) => (f.id_cooperative == c)).map((m) => m._id),
+                      "especes": this.state.Paniers.filter((f) => (f.id_cooperative == c)),
+                      "prix": this.state.Paniers.filter((f) => (f.id_cooperative == c)).reduce(function (prev, cur) { return prev - (- cur.prix); }, 0)
+                    })
+                  });
+                  this.setState({ panier: p  });
+                  const pageNumbers = [];
+                  for (let i = 1; i <= Math.ceil(this.state.panier.length / this.state.annoncesPerPage); i++) {
+                    pageNumbers.push(i);
+                  }
+                  this.setState({ nombrePages: pageNumbers });
+                })));
+
+
           });
       });
     }
   }
-  handleDeleteFromPanier(Mid) {
+  handleDeleteFromPanier(Mid, cid) {
     // const idm = this.props.location.state.id;
     // console.log(Mid);
     const token = localStorage.getItem("usertoken");
@@ -112,13 +138,11 @@ class Commandes extends Component {
      if (!token) {
       this.props.history.push("/login");
     } else {
-     
-       console.log(localStorage.getItem("myToken"));
 
       axios
         .put(
           "http://127.0.0.1:8000/api/consommateur/" + token + "/panier/" + Mid,
-          {  },
+          {},
           {
             headers: {
               //"Content-Type": "application/json",
@@ -127,45 +151,69 @@ class Commandes extends Component {
             },
           }
         )
-        .then(()=>{
+        .then(() => {
           this.setState(
             {
-              Favoris: this.state.Favoris.filter(
-                (Favoris) => Favoris._id !== Mid
+
+              Paniers: this.state.Paniers.filter(
+                (Paniers) => Paniers._id !== Mid
               ),
             });
-            this.props.history.push("/Panier")
 
+          let p = [];
+         
+          this.state.coop.map((c) => {
+            p.splice(0, 0, {
+              "id_coop": c,
+              "nom_coop": this.state.coopn.filter((f) => (f.idc == c)).map((m) => m.nom)[0],
+              "id_espaces": this.state.Paniers.filter((f) => (f.id_cooperative == c)).map((m) => m._id),
+              "especes": this.state.Paniers.filter((f) => (f.id_cooperative == c)),
+              "prix": this.state.Paniers.filter((f) => (f.id_cooperative == c)).reduce(function (prev, cur) { return prev - (- cur.prix); }, 0)
+            })
           });
-          Swal.fire({
-            title: "Supprimé avec succès ",
-            icon: "success",
-            width: 400,
-            heightAuto: false,
-            timer: 1500,
-            showConfirmButton: false,
-            /* confirmButtonColor: "#7fad39",
-    
-            confirmButtonText: "Ok!",*/
-          });
-    
-    
-  }}
+
+          this.setState({ panier: p.filter((f) => f.especes.length >= 1)});
+
+        
+          this.props.history.push("/Panier")
+
+        });
+      Swal.fire({
+        title: "Supprimé avec succès ",
+        icon: "success",
+        width: 400,
+        heightAuto: false,
+        timer: 1500,
+        showConfirmButton: false,
+       
+      });
+      const pageNumbers = [];
+      for (
+        let i = 1;
+        i <=
+        Math.ceil(this.state.panier.filter((f) => f.especes.length >= 1).length / (this.state.annoncesPerPage));
+        i++
+      ) {
+         pageNumbers.push(i);
+      }
+      this.setState({ nombrePages: pageNumbers });
+  
+
+    }
+  }
+  annonceVision(a) {
+    if (a.race === undefined) {
+      return " ";
+    } else return a.race;
+  }
 
   paginate(pageNumber) {
     this.setState({ currentPage: pageNumber });
   }
 
   render() {
-    var fav = this.state.Favoris.filter((Favoris) => Favoris !== null);
-    // console.log(fav);
-    const indexOfLastAnnonce =
-      this.state.currentPage * this.state.annoncesPerPage;
-    const indexOfFirstAnnonce = indexOfLastAnnonce - this.state.annoncesPerPage;
-    const currentAnnonces = this.state.Favoris.slice(
-      indexOfFirstAnnonce,
-      indexOfLastAnnonce
-    );
+    var fav = this.state.Paniers.filter((Paniers) => Paniers !== null);
+ 
     const { loading } = this.state;
 
     let titre;
@@ -188,14 +236,17 @@ class Commandes extends Component {
         {/* <div id="preloder">
            <div className="loader"></div>
         </div>  */}
+
         <section className="">
           <div className="container">
             <br></br>
             <h3 class="latest-product__item">
               Mon Panier <i class="fa fa-shopping-cart"></i>
             </h3>
+
+
             <div className="row">
-              
+
               <div className="col-lg-12 col-md-7">
                 {/*<!-- Sheeps Grid Section Begin --> */}
                 <div className="filter__found text-left">
@@ -223,62 +274,129 @@ class Commandes extends Component {
                     </div>
                   ) : (
                     <div>
-                      <div class="row">
-                        {currentAnnonces.map((Annonces) => (
-                          //  {if(Annonces){}}
-                          <div className="col-lg-3 col-md-3 col-sm-6">
-                            {console.log(Annonces.created_at)}
-                            <div id="anonce" className="product__item">
-                              <div
-                                className="product__item__pic set-bg"
-                                // data-setbg={Annonces.images}
-                                // src="Images/sardi1.jpg"
+                      <div >
+                        {this.state.panier.slice(this.state.currentPage * 2 - 2, this.state.currentPage * 2)
+                          .map((p) => <div class="row border mb-2">
+                            <div className=" col-lg-12 col-md-12 col-sm-12 mt-3">
+                              <b className="text-dark">Cooperative : </b><b className="text-primary">{p.nom_coop}</b>
+                              <Link
+                                to={{
+                                  pathname: "/Commander",
+                                  state: {
+                                    id: p.id_espaces,
+                                  },
+                                }}
                               >
-                                <centre>
-                                  {" "}
-                                  <img
-                                    src={Annonces.image_face}
-                                    className="product__item__pic set-bg"
-                                  />
-                                </centre>
-                                <ul class="product__item__pic__hover">
-                                  <li>
-                                    <Link to={`/DetailsMouton/${Annonces._id}`}>
-                                      <a href="#">
-                                        <i class="fa fa-eye"></i>
-                                      </a>
-                                    </Link>
-                                  </li>
-                                  <li>
-                                    <a
-                                      id={Annonces._id}
-                                      onClick={(e) =>
-                                        this.handleDeleteFromPanier(
-                                          e.currentTarget.id
-                                        )
-                                      }
-                                    >
-                                      <i class="fa fa-trash"></i>
-                                    </a>
-                                  </li>
-                                </ul>
-                              </div>
-                              <div className="product__item__text">
-                                {/* <h6 className="text-danger">{"         " + Annonces.statut}</h6> */}
-                                {/* <h6>{"   Livrer à :      " + Annonces.point_relais}</h6> */}
-                                {/* <h6> {"         " + Annonces.prix + "  MAD"}</h6> */}
-                                <h5>{"         " + Annonces.poids + " Kg"}</h5>
-                                <h5 id="prixFav">
-                                  {"         " + Annonces.prix + " MAD"}
-                                </h5>
-                              </div>
+                                <b className="text-danger float-right">{p.prix} Dhs {" "} <button
+                                  id={p.id_espaces}
+                                  className=" rounded text-white bg-success py-1 px-2 ml-3  "
+                                  style={{ fontSize: "16px", border: "none" }}
+                                //  onClick={(e) => {
+                                //   this.handlePanier(e.currentTarget.id);
+                                //  }
+                                // }
+                                >{""} Commande globale</button>
+                                </b></Link>
                             </div>
-                          </div>
-                        ))}
+
+                            <br></br>
+                            {p.especes.map((Annonces) => (
+                              //  {if(Annonces){}}
+                              <div className="col-lg-3 col-md-3 col-sm-6">
+                                <br></br>
+                                <div id="anonce" className="product__item">
+                                  <div
+                                    className="product__item__pic set-bg"
+                                  // data-setbg={Annonces.images}
+                                  // src="Images/sardi1.jpg"
+                                  >
+                                    <centre>
+                                      {" "}
+                                      <img
+                                        src={Annonces.image_face}
+                                        className="product__item__pic set-bg"
+                                      />
+                                    </centre>
+                                    <ul class="product__item__pic__hover">
+                                      <li>
+                                        <Link to={`/DetailsMouton/${Annonces._id}`}>
+                                          <a href="#">
+                                            <i class="fa fa-eye"></i>
+                                          </a>
+                                        </Link>
+                                      </li>
+                                      <li>
+                                        <a
+                                          id={Annonces._id}
+                                          onClick={(e) =>
+                                            this.handleDeleteFromPanier(
+                                              e.currentTarget.id, p.id_coop
+                                            )
+                                          }
+                                        >
+                                          <i class="fa fa-trash"></i>
+                                        </a>
+                                      </li>
+                                    </ul>
+                                  </div>
+
+                                  {Annonces.anoc ?
+                                    <h1 style={{ borderRadius: "0% 0% 0% 40%", fontSize: "14px" }} class=" badge badge-success pt-2 w-100  ">
+                                      <HiOutlineBadgeCheck className=" mr-1 fa-lg " />
+                                      <span>Labélisé ANOC</span>  </h1>
+                                    :
+                                    <span className="badge pt-3 w-100 mb-2    ">{" "}</span>
+                                  }
+
+                                  <div className="product__item__text p-2 text-justify">
+                                    {/* <h6 className="text-danger">{"         " + Annonces.statut}</h6> */}
+                                    {/* <h6>{"   Livrer à :      " + Annonces.point_relais}</h6> */}
+                                    {/* <h6> {"         " + Annonces.prix + "   Dhs"}</h6> */}
+                                    <h6 ><GiSheep className=" mr-1 fa-lg " />{Annonces.categorie}
+
+
+                                      <span className="float-right">
+                                        <i class="fa fa-dot-circle-o"></i> {this.annonceVision(Annonces)}
+                                      </span> </h6>
+
+                                    <h6><GiWeight className=" mr-1 fa-lg " />
+                                      {Annonces.poids + " Kg"}
+                                      <img style={{ width: "10%", marginLeft: "91px", marginBottom: "10px" }} src="./Images/age.png"></img>
+
+                                      <span className="float-right mt-1">
+                                        {Annonces.age + " mois"}</span></h6>
+
+                                    <h5 id="mad">
+                                      {"         " + Annonces.prix + "  Dhs"}
+                                    </h5>
+                                    {Annonces.statut == "disponible" ?
+                                      <Link
+                                        to={{
+                                          pathname: "/Commander",
+                                          state: {
+                                            id: Annonces._id,
+                                          },
+                                        }}
+                                      > <button
+                                        id={Annonces._id}
+                                        className="float-right rounded text-white bg-success py-1 px-2  "
+                                        style={{ fontSize: "16px", border: "none" }}
+                                      //  onClick={(e) => {
+                                      //   this.handlePanier(e.currentTarget.id);
+                                      //  }
+                                      // }
+                                      >
+                                          {""} Commander
+                                    </button></Link> : null}
+
+                                  </div>
+                                </div>
+                              </div>
+                            ))}</div>)}
                       </div>
                       {/* <!-- Sheeps Grid Section End --> */}
 
-                      <div className="center-div">
+                      { <div className="center-div">
                         <nav className="row">
                           <ul className="pagination center-div">
                             {this.state.nombrePages.map((number) => (
@@ -296,7 +414,7 @@ class Commandes extends Component {
                             ))}
                           </ul>
                         </nav>
-                      </div>
+                      </div>}
                       <br></br>
                     </div>
                   )}
